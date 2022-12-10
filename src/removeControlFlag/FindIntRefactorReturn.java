0a1dec55a3c2eb95653385b92b00452ad144a6f9
KIMSEI1124 <workju1124@gmail.com>
2022-12-10 22:04:00 +0900

    feat: 제어 플래그 삭제 예제 코드 작성

---
+++ src/removeControlFlag/FindIntRefactorReturn.java
@@ -0,0 +1,12 @@
+package removeControlFlag;
+
+public class FindIntRefactorReturn {
+    public static boolean find(int[] data, int target) {
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
