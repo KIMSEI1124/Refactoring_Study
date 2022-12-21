b1c4f56634cfceb4c5db418a3c2281f7c39efbd4
KIMSEI1124 <workju1124@gmail.com>
2022-12-21 15:27:34 +0900

    feat: 4장 널 객체 도입, 예제 프로그램 작성

---
+++ src/introduceNullObject/after/Label.java
@@ -0,0 +1,21 @@
+package introduceNullObject.after;
+
+public class Label {
+    private final String _label;
+
+    public Label(String label) {
+        _label = label;
+    }
+
+    public void display() {
+        System.out.println("display: " + _label);
+    }
+
+    public String toString() {
+        return "\"" + _label + "\"";
+    }
+
+    public boolean isNull() {
+        return false;
+    }
+}
---
+++ src/introduceNullObject/after/NullLabel.java
@@ -0,0 +1,17 @@
+package introduceNullObject.after;
+
+public class NullLabel extends Label {
+
+    public NullLabel() {
+        super("(none)");
+    }
+
+    @Override
+    public void display() {
+    }
+
+    @Override
+    public boolean isNull() {
+        return true;
+    }
+}
