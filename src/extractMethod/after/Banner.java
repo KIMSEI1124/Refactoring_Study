a2224f242eab18349f6bf20f789445bbb4942a4b
KIMSEI1124 <workju1124@gmail.com>
2022-12-25 13:47:47 +0900

    feat: 자바로 배우는 리팩토링 입문 5장 구현

---
+++ src/extractMethod/after/Banner.java
@@ -0,0 +1,29 @@
+package extractMethod.after;
+
+public class Banner {
+    private final String content;
+
+    public Banner(String content) {
+        this.content = content;
+    }
+
+    public void print(int times) {
+        printBorder();
+        printContent(times);
+        printBorder();
+    }
+
+    private void printBorder() {
+        System.out.print("+");
+        for (int i = 0; i < content.length(); i++) {
+            System.out.print("-");
+        }
+        System.out.println("+");
+    }
+
+    private void printContent(int times) {
+        for (int i = 0; i < times; i++) {
+            System.out.println("|" + content + "|");
+        }
+    }
+}
---
+++ src/extractMethod/before/Main.java
@@ -0,0 +1,8 @@
+package extractMethod.before;
+
+public class Main {
+    public static void main(String[] args) {
+        Banner hello = new Banner("Hello, world!");
+        hello.print(3);
+    }
+}
